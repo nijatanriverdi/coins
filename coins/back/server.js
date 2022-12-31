8a07e4cc0046be076e70055ba57c480080091d1f
nijatanriverdi <119072589+nijatanriverdi@users.noreply.github.com>
2022-12-31 17:00:47 +0400

    Add files via upload

---
+++ coins/back/server.js
@@ -0,0 +1,28 @@
+const connection = require("./db/db")();
+
+const app = require('express')();
+
+const cors = require('cors');
+app.use(cors());
+
+app.get('/coins', function (req, res) {
+    connection.query(`SELECT * FROM coins`, (err, data) => {
+        if (err) return res.status(500);
+        res.json(data);
+    });;
+});
+
+app.get('/coins/:id', function (req, res) {
+    const id = +req.params.id;
+    const sql = `SELECT * FROM coins WHERE id=${id}`;
+    connection.query(sql, (err, results, fields) => {
+        if (err) {
+            return res.status(500);
+        }
+        res.json(results);
+    });
+});
+
+app.listen(5000, function () {
+    console.log('Example app listening on port 5000!');
+});
